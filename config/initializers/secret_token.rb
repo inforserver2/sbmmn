6b0f08a13df5157da4bdeacc4ba612156970fc26
root <root@server.mylinuxserver2.info>
2012-12-03 12:46:48 +0000

    base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SbmmnCom::Application.config.secret_token = '6f83a0c498d41552647eea815a77ff8926eed6325d5a5cfc535f2218887e320429cd874ca8473dfa9ddaf6587aa42b5d6e25da44da964f7383bec93ae9678dca'
