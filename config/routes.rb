73ef4a1663ceb4202d2e9b70ad35f5bbb620e710
Humberto Ribeiro <princehrb@gmail.com>
2012-12-03 14:02:58 +0000

    basic auth struct

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 SbmmnCom::Application.routes.draw do
+
+
+  root :to => "home#index"
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :users
+  resources :articles
+  resources :sessions
+
+
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
